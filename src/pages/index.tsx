85240d0346aa7bdc5594c73aa975ef4037ce693e
Huy <106767595+bbi3mn4u69@users.noreply.github.com>
2025-05-09 19:50:05 +1000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -80,7 +80,7 @@ const Home = () => {
 
     const intervalId = setInterval(() => {
       Promise.all([getData()]).catch();
-    }, 3);
+    }, 300);
 
     return () => clearInterval(intervalId);
   }, []);
